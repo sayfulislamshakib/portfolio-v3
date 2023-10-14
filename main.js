f08b16598cd95e5e65a9dcd46b5e52a0970af1b3
Sayful Islam Shakib <connect.sayful@gmail.com>
2023-10-14 16:50:08 +0600

    animation more smooth

--- main.js
+++ main.js
@@ -67,10 +67,10 @@ updateActiveTab();
 $(document).ready(function () {
 	$(".popup-image img").click(function () {
 		$("#full-image").attr("src", $(this).attr("src"));
-		$("#image-viewer").fadeIn(250);
+		$("#image-viewer").fadeIn(400);
 	});
 
 	$("#image-viewer").click(function () {
-		$("#image-viewer").fadeOut(250);
+		$("#image-viewer").fadeOut(400);
 	});
 });

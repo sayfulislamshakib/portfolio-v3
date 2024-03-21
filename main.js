98792cd9c36d08043d56de3d7630ae9c24b6ec16
Sayful Islam Shakib <connect.sayful@gmail.com>
2024-03-21 12:23:31 +0600

    Bug 🐛:- By nav is now by default hidden.

--- main.js
+++ main.js
@@ -95,7 +95,7 @@ function copyText() {
 
 function myFunction() {
   var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
+  if (x.style.display === "none" || x.style.display === "") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
